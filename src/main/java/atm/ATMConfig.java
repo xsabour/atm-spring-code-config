b5578dd243427d069c2d2b8f9537aedf0a3766e1
atm <kritsada.lakhasont@gmail.com>
2020-12-27 19:34:13 +0700

    first commit

---
+++ src/main/java/atm/ATMConfig.java
@@ -0,0 +1,27 @@
+package atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+    public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceDB();
+    }
+
+    @Bean
+    public Bank bank() {
+        return new Bank("My Bank", dataSource());
+    }
+
+    @Bean
+    public ATM atm() {
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return new AtmUI(atm());
+    }
+}
